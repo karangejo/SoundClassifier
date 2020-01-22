b4cdce6d05b2de9da46295df335154becec02493
karang <gabriel.g.gras@gmail.com>
2020-01-22 15:40:13 +0800

    fixed audio recorder

--- recAudio.py
+++ recAudio.py
@@ -16,6 +16,8 @@
 import sys
 import time
 import alsaaudio
+import pyaudio
+import wave
 
 outputfile = 'rec.wav'
 device = 'default'
@@ -45,7 +47,7 @@ def record_audio_to_file(device,outputfile,duration):
     # mode.
     inp.setperiodsize(160)
 
-    loops = 1000000
+    loops = 1000 * duration
     while loops > 0:
         loops -= 1
         # Read data from device
@@ -53,3 +55,94 @@ def record_audio_to_file(device,outputfile,duration):
         if l:
             f.write(data)
             time.sleep(.001)
+
+def rec_audio_to_file(outputfile,duration):
+
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    seconds = duration
+    filename = outputfile
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+def rec_audio_to_file_keyboard_interrupt(outputfile,duration):
+
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    seconds = duration
+    filename = outputfile
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+    '''
+        try to integrate this loop so we can record as long as needed and then press a button to stop
+        try:
+            while True:
+                do_something()
+        except KeyboardInterrupt:
+            pass
+    '''
+    try:
+        while True:
+            data = stream.read(chunk)
+            frames.append(data)
+    except KeyboardInterrupt:
+        # Stop and close the stream
+        stream.stop_stream()
+        stream.close()
+        # Terminate the PortAudio interface
+        p.terminate()
+
+        print('Finished recording')
+
+        # Save the recorded data as a WAV file
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(channels)
+        wf.setsampwidth(p.get_sample_size(sample_format))
+        wf.setframerate(fs)
+        wf.writeframes(b''.join(frames))
+        wf.close()
