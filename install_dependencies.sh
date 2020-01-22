c3462462fdbb2decf7e8d906d1f18b3d42398a2d
karang <gabriel.g.gras@gmail.com>
2020-01-22 09:47:17 +0800

    added .gitignore

---
+++ install_dependencies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+dependencies="python3 python3-pip python3-virtualenv aubio-tools"
+
+check_and_install(){
+	KG_OK=$(dpkg-query -W --showformat='${Status}\n' aubio-tools|grep "install ok installed")
+	echo Checking for aubio-tools: $PKG_OK
+	if [ "" == "$PKG_OK" ]; then
+  		echo "aubio-tools package not found. Setting up aubio-tools."
+  		sudo apt-get install aubio-tools
+	fi
+}
+
+for arg in $dependencies;
+do
+check_and_install $arg
+done
---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# First run:
+
+sudo ./install_dependencies
+
+# then set up a python virtual environment by running:
+
+virtualenv soundClassifier
+
+# after that activate it by running:
+
+./activate
+
+# finally install all the python dependencies by running:
+
+pip3 install -r requirements.txt
+
